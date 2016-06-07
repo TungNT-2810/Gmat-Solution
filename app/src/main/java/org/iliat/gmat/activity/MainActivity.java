800c7564178bc6a73fa368663430a44608b60811
linhdq <bom@1995>
2016-06-07 18:09:12 +0700

    update

--- app/src/main/java/org/iliat/gmat/activity/MainActivity.java
+++ app/src/main/java/org/iliat/gmat/activity/MainActivity.java
@@ -130,7 +130,10 @@ public class MainActivity extends AppCompatActivity
                         {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                finish();
+                                Intent intent = new Intent(Intent.ACTION_MAIN);
+                                intent.addCategory(Intent.CATEGORY_HOME);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
                             }
                         })
                         .setNegativeButton("No", null)
