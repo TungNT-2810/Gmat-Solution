3d1318df34cc87d9d74eb44f6436eafe24a474b8
linhdq <bom@1995>
2016-06-10 10:43:47 +0700

    up

--- app/src/main/java/org/iliat/gmat/fragment/HomeFragment.java
+++ app/src/main/java/org/iliat/gmat/fragment/HomeFragment.java
@@ -23,6 +23,7 @@ import org.iliat.gmat.view_model.QuestionPackViewModel;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by ZYuTernity on 5/16/2016.
@@ -57,21 +58,21 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener,
 
     public void init(View view) {
         recyclerView = (RecyclerView) view.findViewById(R.id.list_pack_question);
-        btnMore=(Button)view.findViewById(R.id.btnMore);
+        btnMore = (Button) view.findViewById(R.id.btnMore);
         btnMore.setOnClickListener(this);
-        arcProgress=(ArcProgress)view.findViewById(R.id.home_arc_progress);
-        txtSkillLevel=(TextView)view.findViewById(R.id.txt_skill_leel);
+        arcProgress = (ArcProgress) view.findViewById(R.id.home_arc_progress);
+        txtSkillLevel = (TextView) view.findViewById(R.id.txt_skill_leel);
     }
 
-    private void calculateSkillLevel(int totalRightAnswer, int totalQuestion){
-        float rate=0;
-        if(totalQuestion>0){
-            rate=(float)totalRightAnswer/totalQuestion;
-            if(rate<70){
+    private void calculateSkillLevel(int totalRightAnswer, int totalQuestion) {
+        float rate = 0;
+        if (totalQuestion > 0) {
+            rate = (float) totalRightAnswer / totalQuestion;
+            if (rate < 70) {
                 txtSkillLevel.setText("Newbie");
-            }else if(rate>=70 && rate<90){
+            } else if (rate >= 70 && rate < 90) {
                 txtSkillLevel.setText("Intermediate");
-            }else{
+            } else {
                 txtSkillLevel.setText("Master");
             }
         }
@@ -84,7 +85,8 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener,
         recyclerView.setLayoutManager(layoutManager);
         ListQuestionPackAdapter listQuestionPackAdapter = new ListQuestionPackAdapter();
         realm = Realm.getDefaultInstance();
-        listQuestionPackAdapter.setQuestionPackList(realm.where(QuestionPackModel.class).findAll());
+        listQuestionPackAdapter.setQuestionPackList(realm.where(QuestionPackModel.class)
+                .findAllSorted("availableTime", Sort.ASCENDING));
         listQuestionPackAdapter.setQuestionPackListener(this);
         listQuestionPackAdapter.setContext(context);
         recyclerView.setAdapter(listQuestionPackAdapter);
@@ -94,33 +96,33 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener,
     }
 
     private void getDataForArcProgress() {
-        int totalQuestion=0;
-        int totalAnswered=0;
-        int totalRightAnswer=0;
+        int totalQuestion = 0;
+        int totalAnswered = 0;
+        int totalRightAnswer = 0;
         results = realm.where(QuestionModel.class).findAll().distinct("id");
         totalQuestion = results.size();
-        results = realm.where(QuestionModel.class).notEqualTo("userAnswer",(-1)).findAll().distinct("id");
+        results = realm.where(QuestionModel.class).notEqualTo("userAnswer", (-1)).findAll().distinct("id");
         totalAnswered = results.size();
         arcProgress.setMax(100);
-        if(totalQuestion!=0) {
+        if (totalQuestion != 0) {
             arcProgress.setProgress(totalAnswered * 100 / totalQuestion);
-        }else{
+        } else {
             arcProgress.setProgress(0);
         }
-        for(int i=0;i<results.size();i++){
-            if(results.get(i).getUserAnswer()==results.get(i).getRightAnswerIndex()){
+        for (int i = 0; i < results.size(); i++) {
+            if (results.get(i).getUserAnswer() == results.get(i).getRightAnswerIndex()) {
                 totalRightAnswer++;
             }
         }
-        calculateSkillLevel(totalRightAnswer,totalQuestion);
+        calculateSkillLevel(totalRightAnswer, totalQuestion);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnMore:{
+            case R.id.btnMore: {
                 getScreenManager().setTitleOfActionBar("Summary");
-                getScreenManager().openFragment(new SummaryFragment(),true);
+                getScreenManager().openFragment(new SummaryFragment(), true);
                 break;
             }
             default:
